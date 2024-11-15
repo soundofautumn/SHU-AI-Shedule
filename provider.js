65892ba8f5f813dc2e4ece6a3d9f198f489882d0
soundofautumn <qjm1412@163.com>
2024-11-15 23:51:59 +0800

    provider和timer

--- provider.js
+++ provider.js
@@ -1,4 +1,26 @@
-function scheduleHtmlProvider() {
-    return document.querySelector('div.select-course-main').innerHTML;
-
+async function scheduleHtmlProvider() {
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+  const token = getCookie("Authorization");
+  const ref = window.location.href;
+  const batchId = ref.match(/batchId=(\w+)/)[1];
+  const res = await fetch("https://jwxk.shu.edu.cn/xsxk/elective/shu/xskb", {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      authorization: token,
+      batchid: batchId,
+    },
+    referrer: ref,
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: "",
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+  });
+  const data = await res.json();
+  return JSON.stringify(data.data);
 }
--- timer.js
+++ timer.js
@@ -1,3 +1,74 @@
-async function scheduleTimer({ providerRes, parserRes } = {}) {
-    
+async function scheduleTimer() {
+  return {
+    totalWeek: 10, // 总周数：[1, 30]之间的整数
+    //TODO: 开学时间
+    startSemester: "", // 开学时间：时间戳，13位长度字符串，推荐用代码生成
+    startWithSunday: false, // 是否是周日为起始日，该选项为true时，会开启显示周末选项
+    showWeekend: false, // 是否显示周末
+    forenoon: 4, // 上午课程节数：[1, 10]之间的整数
+    afternoon: 4, // 下午课程节数：[0, 10]之间的整数
+    night: 4, // 晚间课程节数：[0, 10]之间的整数
+    sections: [
+      {
+        section: 1, // 节次：[1, 30]之间的整数
+        startTime: "08:00", // 开始时间：参照这个标准格式5位长度字符串
+        endTime: "08:45", // 结束时间：同上
+      },
+      {
+        section: 2,
+        startTime: "08:55",
+        endTime: "09:40",
+      },
+      {
+        section: 3,
+        startTime: "10:00",
+        endTime: "10:45",
+      },
+      {
+        section: 4,
+        startTime: "10:55",
+        endTime: "11:40",
+      },
+      {
+        section: 5,
+        startTime: "13:00",
+        endTime: "13:45",
+      },
+      {
+        section: 6,
+        startTime: "13:55",
+        endTime: "14:40",
+      },
+      {
+        section: 7,
+        startTime: "15:00",
+        endTime: "15:45",
+      },
+      {
+        section: 8,
+        startTime: "15:55",
+        endTime: "16:40",
+      },
+      {
+        section: 9,
+        startTime: "18:00",
+        endTime: "18:45",
+      },
+      {
+        section: 10,
+        startTime: "18:55",
+        endTime: "19:40",
+      },
+      {
+        section: 11,
+        startTime: "20:00",
+        endTime: "20:45",
+      },
+      {
+        section: 12,
+        startTime: "20:55",
+        endTime: "21:40",
+      },
+    ], // 课程时间表，注意：总长度要和上边配置的节数加和对齐
+  };
 }
